bbec397a53e9c547f3d7aa63a81a9767cc5e3e33
AliciadW <alicia.dewet@living-stratus.com>
2018-10-23 13:15:23 +0200

    Fixed spacing for hero title

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -15,7 +15,10 @@ const Hero = ({ classes }) => {
           Resources for
         </Typography>
         <Typography variant="display4" component="span" className={largeText}>
-          &nbsp;organising a protest
+          organising
+        </Typography>
+        <Typography variant="display4" component="span" className={largeText}>
+          a protest
         </Typography>
       </h1>
     </div>
