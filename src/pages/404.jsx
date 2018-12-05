be6886a10d799f9a145450aeace2b44230253056
AliciadW <alicia.dewet@living-stratus.com>
2018-12-05 13:49:42 +0200

    404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
 import BaseLayout from '../components/BaseLayout';
-import Hero404 from '../components/Hero404';
+import Hero from '../components/Hero';
 import SectionsList from '../components/SectionsList';
 import Footer from '../components/Footer';
 
-
 const notFound = () => (
   <BaseLayout>
-    <Hero404 />
+    <Hero
+      title='This resource is still being developed'
+      main = {{
+        content: 'Check back soon',
+        class: 'largeText404'
+      }}
+      buttonContent = {{
+        text: 'Visit the homepage',
+        url: '/',
+        target: '_self'
+      }}
+      paragraph = 'We are currently working hard to develop and publish this resource! Please check back soon or join our mailing list to receive updates about this guide.'
+    />
     <SectionsList />
     <Footer />
   </BaseLayout>
