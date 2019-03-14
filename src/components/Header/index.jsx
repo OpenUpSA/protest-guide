97439ed0c81a264786e0c3b1c8381ae6cea67d8d
Mish Siwela <mishuct26@gmail.com>
2019-03-14 13:40:31 +0200

    Provisional logo-images in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,23 +2,43 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import logo from '../../images/r2p-logo.jpg';
+import logo from '../../images/ndifuna-image-logo.png';
 import SpeedDials from '../SpeedDial';
+import styled from 'styled-components';
 
 import addStyles from './styles';
 
+const HeaderContainer = styled.div`
+  display: flex;
+  margin-left: 0%;
+`
+
+const LogoWrapper = styled.div`
+  display: flex;
+  width: 69.3%;
+  height: 90%;
+  margin-bottom: 0px;
+`
 
 const Header = ({ classes }) => {
   const { root, content, button, buttonLogo, } = classes;
 
   return (
-    <AppBar position="static" color="default" className={root}> 
+    <AppBar position="static" color="default" className={root}>
       <Toolbar className={content}>
-        <div>
+       <HeaderContainer>
+        <LogoWrapper>
           <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
             <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
           </a>
-        </div>
+           <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
+            <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
+          </a>
+          <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
+            <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
+          </a>
+        </LogoWrapper>
+        </HeaderContainer>
       </Toolbar>
       <SpeedDials />
     </AppBar>
