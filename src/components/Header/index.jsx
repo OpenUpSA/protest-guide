aa8e4d5d0a901213be4b850dcde6b17ca6af5113
Mish Siwela <mishuct26@gmail.com>
2019-03-15 11:34:16 +0200

    Header logo name addition

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import logo from '../../images/ndifuna-image-logo.png';
+import landLogo from '../../images/ndifuna-image-logo.png';
+import openupLogo from '../../images/openup-logo.png';
+import land4pLogo from '../../images/land-for-people-logo.png';
+// import IconButton from '@material-ui/core/IconButton';
 import SpeedDials from '../SpeedDial';
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import addStyles from './styles';
 
@@ -29,18 +33,20 @@ const Header = ({ classes }) => {
        <HeaderContainer>
         <LogoWrapper>
           <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
-            <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
+            <Button className={button}><img src={landLogo} className={buttonLogo} alt="ndifuna-image Logo"></img></Button>
           </a>
            <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
-            <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
+            <Button className={button}><img src={land4pLogo} className={buttonLogo} alt="land4people Logo"></img></Button>
           </a>
           <a href='https://www.r2p.org.za/' rel="noopener noreferrer" target='_blank'>
-            <Button className={button}><img src={logo} className={buttonLogo} alt="right2protest Logo"></img></Button>
+            <Button className={button}><img src={openupLogo} className={buttonLogo} alt="openup Logo"></img></Button>
           </a>
-        </LogoWrapper>
+          </LogoWrapper>
         </HeaderContainer>
+        <MenuIcon />
       </Toolbar>
       <SpeedDials />
+      {/* <IconButton /> */}
     </AppBar>
   )
 };
